c4de359de5797db287ee1fde928f0e7ced3d212e
nyhryan <nyhryan99102@gmail.com>
2023-08-16 18:08:19 +0900

    added neovim setups

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,21 @@
+local set = vim.opt
+set.clipboard = 'unnamedplus'
+set.completeopt = {'menu', 'menuone', 'noselect'}
+set.mouse = 'a'
+
+set.tabstop = 4		        	-- number of visual spaces per TAB
+set.softtabstop = 4	        	-- number of spaces in tab when editing
+set.shiftwidth = 4		        -- insert 4 spaces on a tab
+set.expandtab = true		    -- tabes are spaces
+
+set.number = true
+set.relativenumber = true
+set.cursorline = true
+set.splitbelow = true
+set.splitright = true
+set.termguicolors = true
+
+set.incsearch = true
+set.hlsearch = false
+set.ignorecase = true
+set.smartcase = true
