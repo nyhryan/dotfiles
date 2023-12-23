8cdada69fd3902b9ceebdc9e49b1c1b63a9ef578
ATAI <nyhryan99102@gmail.com>
2023-12-23 20:01:35 +0900

    edtied lua/colorscheme

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -2,6 +2,6 @@ local colorscheme = 'catppuccin'
 
 local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not is_ok then
-    vim.notify('colorscheme' .. colorscheme .. 'not found!')
+    vim.notify('colorscheme ' .. colorscheme .. 'not found!')
     return
 end
