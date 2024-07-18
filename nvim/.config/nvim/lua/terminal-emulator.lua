f316c4c36f2ee1ce49b4d87ec1eaf7a2d05f532d
ATAI <nyhryan99102@gmail.com>
2024-07-18 16:28:59 +0900

    nvim kickstarter added

--- nvim/.config/nvim/lua/terminal-emulator.lua
+++
@@ -1,9 +0,0 @@
-local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
-}
-
-vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', opts)
-
-vim.api.nvim_command('autocmd TermOpen * startinsert')
-vim.api.nvim_command('autocmd TermOpen * setlocal nonumber norelativenumber')
